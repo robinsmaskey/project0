84f9d3eb25c9ac9474de081853d34d9c7f6fc0ef
robinsmaskey <rmaske45@gmail.com>
2021-03-30 22:07:32 +0545

    My very first commit

---
+++ mysite/urls.py
@@ -0,0 +1,37 @@
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from personal.views import home_screen_view
+from account.views import registration_view, logout_view, login_view, account_view, must_authenticate_view
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home_screen_view, name = "home"),
+    path('blog/', include('blog.urls','blog')),
+    path('register/',registration_view, name = 'register'),
+    path('logout/',logout_view, name = 'logout'),
+    path('login/',login_view, name = 'login'),
+    path('account/',account_view, name = 'account'),
+    path('must_authenticate/', must_authenticate_view, name = "must_authenticate"),
+
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ personal/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from blog.models import BlogPost
+from operator import attrgetter
+from blog.views import get_blog_queryset
+# Create your views here.
+
+def home_screen_view(request):
+	context = {}
+
+	query = ""
+	if request.GET:
+		query = request.GET['q']
+		context['query'] = str(query)
+
+	blog_posts = sorted(get_blog_queryset(query),key=attrgetter('date_updated'), reverse=True)
+	context['blog_posts'] = blog_posts
+	
+	return render(request,"personal/home.html", context)
